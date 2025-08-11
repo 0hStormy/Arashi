0cb2b0f76794fb21835e9ebd05584787232c87b7
Stormy <80485413+0hStormy@users.noreply.github.com>
2025-08-11 08:11:36 -0700

    Add release archive generator

---
+++ dist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+release="25.08.4"
+
+# Create copy of working directory for distribution
+mkdir "Arashi-${release}"
+rsync -av --progress ./ "Arashi-${release}/" --exclude "Arashi-${release}" # Copy all but new folder
+
+# Remove non-essential files/folders
+cd "Arashi-${release}"
+rm -rf .git # Has to be -rf to remove protected objects made by git
+rm -r .github
+rm -r dist.sh
+cd ..
+
+# Create release archive
+zip -r "Arashi-${release}.zip" "Arashi-${release}"
+mkdir -p dist
+mv "Arashi-${release}.zip" "dist/Arashi-${release}.zip"
+
+# Clean-up
+rm -r "Arashi-${release}"
+
+# Done
+echo -e "\033[0;32mFinished!"
